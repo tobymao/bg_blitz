45ab24859192b771ff6947014e4243c39264634a
Toby Mao <toby.mao@gmail.com>
2016-05-22 14:21:09 -0700

    google analytics

--- views/page.rb
+++ views/page.rb
@@ -19,6 +19,7 @@ module Views
           link rel: 'shortcut icon', type: 'image/png', href: '/images/favicon.ico'
           render_head
           render_style
+          render_analytics
           meta name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0'
           meta name: 'apple-mobile-web-app-capable', content: 'yes'
         end
@@ -99,6 +100,19 @@ module Views
       CSS
     end
     static :render_style
+
+    def render_analytics
+      script <<~JS
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-78138575-1', 'auto');
+      ga('send', 'pageview');
+      JS
+    end
+    static :render_analytics
 
     def render_banner
       widget Banner
