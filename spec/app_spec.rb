6b829b3743f1bac6a657ae85b9987d260f0dfca4
Toby Mao <toby.mao@gmail.com>
2016-05-23 19:29:15 -0700

    updated bios. add more specs

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,11 +3,35 @@ require './spec/spec_helper'
 describe BGBlitz do
   let(:app) { BGBlitz.freeze.app }
 
+  def auth_header
+    user = ENV['AUTH_USER']
+    pass = ENV['AUTH_PASS']
+    { 'HTTP_AUTHORIZATION' => 'Basic ' + Base64::encode64("#{user}:#{pass}") }
+  end
+
   describe 'get requests' do
-    %w[/ /podcasts /videos /blog].each do |path|
+    %w[/ /podcasts /videos /blog /about /contact /rss.xml].each do |path|
       it 'should return 200' do
         get path
         expect(last_response).to be_ok
+      end
+    end
+  end
+
+  describe 'admin' do
+    paths = %w[admin/items admin/posts]
+
+    paths.each do |path|
+      it 'should return 200 with auth' do
+        get path, {}, auth_header
+        expect(last_response).to be_ok
+      end
+    end
+
+    paths.each do |path|
+      it 'should return 403 without auth' do
+        get path
+        expect(last_response.status).to eq 401
       end
     end
   end
--- views/about.rb
+++ views/about.rb
@@ -24,22 +24,32 @@ module Views
 
       cassadi_icons = [
         Icons::Icon.new('bgg', 'https://boardgamegeek.com/user/noajayne', 'BGG'),
+        Icons::Icon.new('twitter', 'https://twitter.com/noajayne', 'Twitter'),
       ]
 
       render_box 'Cassadi', BGB_PINK, cassadi_icons, 205, 171
 
       div style: text_style do
-        text "Cassadi bio coming soon! "\
+        text "Cassadi has been board gaming for roughly 10 years. Though, the acquisition disorder didn't set in until 2 years ago. "\
+          "Since then she has played hundreds of different games, and found she enjoys teaching new players almost as much as she "\
+          "enjoys playing her favorites. Some at the top of the list are Carcassonne, Five Tribes, Tokaido, and Puerto Rico. She will "\
+          "try most any game once! Other than board gaming Cassadi enjoys being a local craft beer snob and spending time with her family."
       end
 
       crystal_icons = [
         Icons::Icon.new('bgg', 'https://boardgamegeek.com/user/CrysP', 'BGG'),
+        Icons::Icon.new('twitter', 'https://twitter.com/CrystalPisano', 'Twitter'),
       ]
 
       render_box 'Crystal', BGB_PURPLE, crystal_icons, 69, 35
 
       div style: text_style do
-        text "Crystal bio coming soon! "\
+        text "From humble beginnings of Battleship, Kerplunk, Dream Phone, Arch Rival, and Mall Madness... the obsession started early. "\
+          "Crystal has been gaming her whole life, but it wasn't until she was invited to game night at a friend's house in 2007 that "\
+          "she was introduced to modern board games. Early favorites included Betrayal at House on the Hill, Saint Petersburg, and Runebound. "\
+          "Crystal's personal game collection has grown in the years since and she enjoys a wide range of games, particularly those where the "\
+          "theme is well implemented. Battlestar Galactica is her favorite, but her more recent loves include Pandemic Legacy and T.I.M.E. "\
+          "Stories. She'll tell you she hates euro games, but she's willing to give any game a shot if it's with the right group."
       end
     end
     static :render_main
