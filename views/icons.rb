eeb23af50c4cfcfde4ed7b70ed3a2faf611dcb69
Toby Mao <toby.mao@gmail.com>
2016-06-26 00:29:29 -0700

    change youtube url

--- views/icons.rb
+++ views/icons.rb
@@ -8,8 +8,8 @@ module Views
       Icon.new('rss', '/rss.xml', 'RSS'),
       Icon.new('facebook', 'https://www.facebook.com/BGBlitz', 'Facebook'),
       Icon.new('twitter', 'https://twitter.com/bgameblitz', 'Twitter'),
-      Icon.new('google', 'https://google.com', 'Google', true),
-      Icon.new('youtube', 'https://www.youtube.com/channel/UC-zQPrSCinOtta_INepZo_A', 'YouTube'),
+      Icon.new('google', 'https://plus.google.com/+boardgameblitz', 'Google', true),
+      Icon.new('youtube', 'https://www.youtube.com/c/boardgameblitz', 'YouTube'),
       Icon.new('itunes', 'https://itunes.apple.com/us/podcast/board-game-blitz/id1117729882?mt=2', 'iTunes'),
       Icon.new('bgg', 'https://boardgamegeek.com/guild/2581', 'BGG Guild'),
     ]
