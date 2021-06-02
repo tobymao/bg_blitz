803df0e684c834b3b09e3314606955f2e0de9cb5
tobymao <toby.mao@gmail.com>
2021-06-01 18:59:03 -0700

    update google analytics

--- views/page.rb
+++ views/page.rb
@@ -17,6 +17,7 @@ module Views
           link rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700'
           link rel: 'stylesheet', type: 'text/css', href: '/vendor/reset.min.css'
           link rel: 'shortcut icon', type: 'image/png', href: '/images/favicon.ico'
+          script src: 'https://www.googletagmanager.com/gtag/js?id=UA-78138575-1', async: true
           render_head
           render_style
           render_analytics
@@ -135,12 +136,10 @@ module Views
 
     def render_analytics
       script <<~JS
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-78138575-1', 'auto');
-        ga('send', 'pageview');
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-78138575-1');
       JS
     end
     static :render_analytics
