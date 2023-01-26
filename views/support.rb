66fb3e7e5e52cc018a86d6e0398a932909d0483f
tobymao <toby.mao@gmail.com>
2023-01-25 18:50:25 -0800

    support

--- views/support.rb
+++ views/support.rb
@@ -82,8 +82,8 @@ module Views
       div style: default_container_style(text_align: 'center') do
         h1 'Support Us'
 
-        a href: 'https://www.patreon.com/boardgameblitz', target: '_blank' do
-          img src: '/images/patreon.png', style: inline(height: '45px')
+        a href: 'https://ko-fi.com/boardgameblitz', target: '_blank' do
+          img src: 'https://storage.ko-fi.com/cdn/brandasset/kofi_s_tag_white.png', style: inline(height: '45px')
         end
 
         div style: inline(margin: '10px') do
