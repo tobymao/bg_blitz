fd79c32d7f4b0bb36a7191091101e34ec6f4bb70
tobymao <toby.mao@gmail.com>
2023-04-15 21:53:24 -0700

    support

--- views/support.rb
+++ views/support.rb
@@ -2,82 +2,6 @@ require './views/page'
 
 module Views
   class Support < Page
-    SUPPORTERS = [
-      'Adam Brocker',
-      'Adrienne Dong',
-      'Andrew Wilson',
-      'bac68',
-      'Barbara',
-      'Ben Cook',
-      "Ben O'Steen",
-      'Board Game Barrage',
-      'Brian Ashmore',
-      'Catherine Wiener',
-      'Charles Albrecht',
-      'Charles Beauvais',
-      'Chris Ferejohn',
-      'Chris Imershein',
-      'Chris Sasaki',
-      'Chris Zebley',
-      'Clay Crucible Games',
-      'David Briel',
-      'Dave Refici (@FrozenHoHos)',
-      'Drew Hicks',
-      'Eric & Amber Davila',
-      'Evan Schmalz',
-      'Flip the Table',
-      'Geoff Mathews',
-      'George Stankow',
-      'Graham Glaser',
-      'Greg Dickson',
-      'Harley Winfrey',
-      'Jae Malloy',
-      'Jayson Smith',
-      'Jen E',
-      'Jenn B',
-      'Jessica Barnes',
-      'Jim K',
-      'Joe C',
-      'John Munsch',
-      'Jon Paul',
-      'Joseph Orso',
-      'Josh Laison',
-      'Joshua Lee',
-      'Joshua Starr',
-      'Justin Difazzio',
-      'Karen Cleveland',
-      'Katrina Baker',
-      'Kayla Nimis',
-      'Keith Jones',
-      'Kent Parker',
-      'Kevin Russ',
-      'Lindsay Grossmann',
-      'Luyi Stork',
-      'Mark Cruzan-Rea',
-      'MaryBeth Breslin',
-      'Marylou Holly',
-      'Matt Maurice',
-      'Matthew Jameson',
-      'Matthew Young',
-      'Merely Rachel',
-      'Micah Liebert',
-      'Miguel Manansala',
-      'Nicholas Baker',
-      'Olivia Lothary',
-      'Pamela Wagner',
-      'Rattlebox Games',
-      'Ray Taylor',
-      'Rosco Schock',
-      'Scott',
-      'Spencer Hopkins',
-      'Terrence',
-      'Trey Radu-Blackburn',
-      "What's Eric Playing",
-      'William Carrigan',
-      'Winter Blair',
-      'Xoe Allred',
-    ].sort.freeze
-
     def render_main
       div style: default_container_style(text_align: 'center') do
         h1 'Support Us'
@@ -91,16 +15,6 @@ module Views
 
           a href: 'https://www.redbubble.com/people/boardgameblitz/shop', target: '_blank' do
             text 'merchandise on Redbubble!'
-          end
-        end
-
-        h1 style: inline(margin: '20px 0 20px 0', font_size: '25px') do
-          text 'Thanks To Our Current Patrons'
-        end
-
-        SUPPORTERS.each do |name|
-          div do
-            text name
           end
         end
       end
